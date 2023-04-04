73edcb0a83cade4f54f892bbd9ac66bec1330540
Felipe Holanda <felipe.nether@icloud.com>
2023-04-04 11:59:28 -0300

    feat: set routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from "react-router-dom"
+
+export default function MakeRoutes(): JSX.Element {
+    return (
+        <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+        </Routes>
+    )
+}
