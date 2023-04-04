67000aab40fa58b6c6cf5c7c22b39526d525dc10
Felipe Holanda <felipe.nether@icloud.com>
2023-04-04 12:00:07 -0300

    feat: added chakra provider and browserRoutes

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import MakeRoutes from "./routes"
+import { ChakraProvider } from "@chakra-ui/react"
+import { theme } from "./style/theme"
+
+function App() {
+  return (
+    <>
+      <ChakraProvider
+        theme={theme}
+        resetCSS={true}
+        portalZIndex={40}
+      >
+        <MakeRoutes />
+      </ChakraProvider>
+    </>
+  )
+}
+
+export default App
