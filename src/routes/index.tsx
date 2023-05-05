2d06ea2b15516161177e2ecb8385857c1f806d14
Juan Sgarbi <juan.sgarbi1@hotmail.com>
2023-05-05 10:31:26 -0300

    fixin some bugs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,10 +5,6 @@ import { Profile } from "../pages/profile";
 import { RegisterUser } from "../pages/registerUser";
 import { LoginUser } from "../pages/loginUser";
 import { Users } from "../pages/anotherProfile";
-// import ProtectedRoutes from "../components/protectedRoutes";
-import { useEffect, useState } from "react";
-import { useUser } from "../context/user.context";
-import { Flex, Spinner } from "@chakra-ui/react";
 import { ResetPassword } from "../pages/resetPassword";
 
 export default function MakeRoutes(): JSX.Element {
@@ -18,7 +14,7 @@ export default function MakeRoutes(): JSX.Element {
         <Route path="/" element={<Home />} />
         <Route path="/announcement/:id" element={<AnnouncementDetail />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/users/:id" element={<Users/>}/>
+        <Route path="/users/:id" element={<Users />} />
         <Route path="/register" element={<RegisterUser />} />
         <Route path="/login" element={<LoginUser />} />
         <Route path="/resetPassword/:id" element={<ResetPassword />} />
