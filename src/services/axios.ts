d93ff02b0ceb330e5f13f239dc68b6af79caf611
Danilo Cardoso <levitano@hotmail.com>
2023-05-05 11:51:38 -0300

    upd: baseURL api for production

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://back-kars-04.onrender.com",
 });
 
 export const fipeApi = axios.create({
